d0455c7bbc86ba157ee6b2f1b2e7db5c1e89d4d9
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-02-16 09:19:45 +0530

    Add files via upload

---
+++ Lecture 5.2/incriment.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class A
+{
+	protected :
+		int a,b;
+		public :
+		void set()
+		{
+			cout<<"Enter the value of A :";
+			cin >> a;
+			
+			cout<<"Enter the value of B :";
+			cin >> b;
+		}
+
+		void get()
+		{
+			
+			cout <<"A is :"<<a<<endl;
+			cout <<"B is :"<<b<<endl;	
+		}
+		A operator +(A &m2)
+		{
+			A temp;
+			int x = this->a + m2.a;
+			int y = this->b + m2.b;
+			
+			temp.a = x;
+			temp.b = y;
+			
+			return temp;
+		}
+};
+int main()
+{
+	A m1,m2,m3;
+	
+	m1.set();
+	m2.set();
+	
+	m3 = m1 + m2;
+	
+	m3.get();
+	
+	return 0;
+	
+}
